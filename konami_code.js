b9d56e3625650c366059f0d69b99d121c7af9ac0
Jeff Johnson <johnsonjp@yahoo.com>
2019-01-02 15:01:39 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,28 @@
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
 function init() {
-  var index = 0;
-  document.addEventListener('keydown', function (e) {
-    const key = parseInt(e.detail || e.which);
+  //
+  var index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler);
 
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log('key', key)
     if (key === code[index]) {
-      index++;
+      index++
 
-      if (index === code.length - 1) {
-        window.alert("YOU DID IT!");
-        index = 0;
+      if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
       }
     } else {
-      index = 0;
+      index = 0
     }
-  }, false);
+  }
 }
+
+
+
+init();
